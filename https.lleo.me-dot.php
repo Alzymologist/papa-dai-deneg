338f8c44bb935369724f21cc20820528300fcb5a
Leonid Kaganov <lleo@lleo.me>
2023-09-14 00:39:59 +0300

    major: init

--- https.lleo.me-dot.php
+++ https.lleo.me-dot.php
@@ -5,26 +5,27 @@
     $json=(array)json_decode($s);
 
     // если ошибка
-    if(empty($json)) er('json','wrong input');
+    if(empty($json)) er('json','wrong input'); // {"error":"json", "error_message":"wrong input"}
     $price=$json['price'];
     $OrderID=$json['OrderID'];
     $about=$json['about'];
 
     // если нулевая цена
-    if($json['price']==0) er('json','wrong input');
+    if($json['price']==0) er('json','wrong price'); // {"error":"json", "error_message":"wrong price"}
 
     // иногда платеж уже готов
-    if(rand(0,10)>5) ok('payd',array()); // платеж прошел
+    if(rand(0,10)>5) ok('payd',array()); // {"result":"payd"}
 
     // а иногда вот вам адрес для оплаты
-    ok('ready',array('account'=>'0xd619041b935c9697d37259e408b1de0e3f5fd9663f49e1b4654bbae1e0fe6962')); // сделай платеж по адресу
+    ok('ready',array('account'=>'0xd619041b935c9697d37259e408b1de0e3f5fd9663f49e1b4654bbae1e0fe6962'));
+// {"result":"ready","account":"0xd619041b935c9697d37259e408b1de0e3f5fd9663f49e1b4654bbae1e0fe6962"}
 
 
 function er($err,$mes) { send(array('error'=>$err,'error_message'=>$mes)); }
 function ok($res,$a) { $a['result']=$res; send($a); }
 function send($a) {
     $a['version']=$GLOBALS['version'];
-    $add=explode(' ',"wversion price OrderID about"); foreach($add as $l) if(isset($GLOBALS[$l])) $a[$l]=$GLOBALS[$l];
+    $add=explode(' ',"version price OrderID about"); foreach($add as $l) if(isset($GLOBALS[$l])) $a[$l]=$GLOBALS[$l];
     header('Content-Type: application/json');
     die(json_encode($a,JSON_UNESCAPED_UNICODE));
 }
